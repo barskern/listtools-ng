269bd62e365b2431c54c054bb4f2fb2acaf0dd68
Ole Martin Ruud <barskern@outlook.com>
2019-01-25 23:39:53 +0100

    Oppdater dokumentasjon

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ![](https://img.shields.io/badge/status-under--utvikling-orange.svg)
 
-**listtools-ng** er et bibliotek bestående av en headerfil for å håndtere ulike
-typer lister og køer. Hovedmålet med biblioteket er å gi en **enkel**, **minimal**
-og **elegant** abstraksjon over ulike typer lister og køer.
+**listtools-ng** er et bibliotek bestående av en headerfil som implementerer
+en enkel lenket liste. Hovedmålet med biblioteket er å gi en **enkel**,
+**minimal** og **elegant** abstraksjon over en lenket liste.
--- listtools-ng.h
+++ listtools-ng.h
@@ -2,27 +2,55 @@
 #define LISTTOOLS_NG_INCLUDE_ONCE
 
 /*! \file listtools-ng.h
- *  \brief Implementasjon av ulike typer lister og køer
+ *  \brief Implementasjon av en enkel lenket liste
  *
- *  Denne filen inneholder alle implementasjonene i dette biblioteket for å
- *  gjøre det enkelt å inkludere i andre prosjekter. Se hver enkelt liste for
- *  eksempler på hvordan de skal brukes.
+ *  Denne filen inneholder hele implementasjonen av listen i for å gjøre det
+ *  enkelt å inkludere biblioteket in andre prosjekter. Alt som trengs for å
+ *  inkludere `listtools-ng` i et annet prosjekt er `#include "listtools-ng.h"`.
+ *
  */
 
-//! En liste med elementer som ligger spredt i minne.
+//! En lenket liste
 
-//! Listen sørger for at alle elementene er linket sammen ved at et element
-//! inneholder en peker til det neste elementet.
+//! Listen sørger for at alle elementene er linket sammen ved at hvert element
+//! i listen inneholder en peker til det neste elementet. Listen kan brukes som
+//! en [FIFO-kø (First In First Out)](https://en.wikipedia.org/wiki/FIFO_(computing_and_electronics)) og en
+//! [stack/LIFO-kø (Last In First Out)](https://en.wikipedia.org/wiki/Stack_(abstract_data_type)).
 //!
-//! Listen er generisk over typen til verdiene den holder, det vil si at man
-//! kan definere listen for mange ulike datatyper, uten at man trenger å
-//! dynamisk holde styr på innholdet. Fordelen med dette er at vi får et
-//! tryggere API som er vanskelig å misbruke. Se eksemplene for å se hvordan
-//! listen kan brukes.
+//! Listen er generisk over typen `T`, altså de ulike verdiene den kan holde.
+//! Det vil si at hvis man vil definere en liste med heltall, så gjøres det med
+//! `List<int>`. Videre kan man gjøre det samme for alle andre typer i C++.
+//! Fordelen ved å bruke en generisk type, er at vi slipper å holde styr på
+//! hvilken type en liste inneholder. Kompilatoren sørger for at hvis man
+//! definerer en liste som inneholder heltall, så kan man kun legge til heltall
+//! i listen. Dersom man prøver å legge til et flyttall i en liste med heltall
+//! vil man få en kompilasjonsfeil. Resultatet av dette er at vi får et trygt
+//! API som er vanskelig å misbruke.
+//!
+//! ## Eksempel
+//!
+//! ```cpp
+//! int main(){
+//!     List<float> list;
+//!
+//!     list.push_front(5);
+//!     cout << *list.front() << endl;
+//!
+//!     list.push_front(10);
+//!     cout << *list.front() << endl;
+//! }
+//! ```
+//!
+//! #### Utgangsverdier
+//!
+//! ```
+//! 5
+//! 10
+//! ```
 template <class T> class List {
 
   //! En node i listen som inneholder en verdi og en peker til den neste
-  //! node in listen
+  //! noden in listen
   struct Node {
     //! Verdien til denne noden
     T value;
@@ -37,34 +65,44 @@ private:
   Node *tail;
 
 public:
+  //! Lager en ny tom liste
   List() : head(nullptr), tail(nullptr) {}
+  //! Sletter alle nodene i listen fra minne
   ~List() {
     Node *current = this->head;
-    // Iterer over alle nodene i listen og fjern dem fra minne
+    // Itererer over alle nodene i listen og fjerner dem fra minne
     while (current != nullptr) {
       Node *tmp = current->next;
       delete current;
       current = tmp;
     }
   }
+
   //! Legg til et nytt element foran i listen
+
+  //! \param new_value verdien som skal legges foran i listen
   void push_front(T new_value) {
-    // Lag en ny node som peker på det nåværende hodet av listen, og sett den
-    // nye noden som det nye hodet av listen vår
-    this->head = new Node{
+    // Lag en ny node som peker på det nåværende hodet av listen
+    Node *new_head = new Node{
         new_value,
         this->head,
     };
-    // Hvis vi ikke allerede har en hale (listen er tom), så er den nåværende
-    // noden både hode og halen
+    this->head = new_head;
+
+    // Hvis halen er null (listen er tom), så er den nye noden både hode og hale
     if (this->tail == nullptr) {
-        this->tail = this->head;
+      this->tail = this->head;
     }
   }
+
   //! Se på det første elementet i listen, hvis det eksisterer
-  T* front() {
-      if (this->head == nullptr) return nullptr;
-      return &this->head->value;
+
+  //! \return enten en peker til den første verdien eller en `nullptr` hvis listen
+  //! er tom
+  T *front() {
+    if (this->head == nullptr)
+      return nullptr;
+    return &this->head->value;
   }
 };
 
