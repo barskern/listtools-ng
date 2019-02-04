778e19944bfc3cb32562bea546a021dedb3485b3
Ole Martin Ruud <barskern@outlook.com>
2019-02-04 22:06:41 +0100

    Formattere kildekoden til testene

--- tests/list_test.cpp
+++ tests/list_test.cpp
@@ -1,75 +1,76 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main()
 #include "../lib/catch.hpp"
 #include "../listtools-ng.h"
 
-SCENARIO( "elementer kan legges til foran i listen og man kan se på dem", "[List]" ) {
+SCENARIO("elementer kan legges til foran i listen og man kan se på dem",
+         "[List]") {
 
-    GIVEN( "En tom liste" ) {
-        List<int> list;
+  GIVEN("En tom liste") {
+    List<int> list;
 
-        REQUIRE( !list.front().has_value() );
+    REQUIRE(!list.front().has_value());
 
-        WHEN( "et element legges til foran i listen" ) {
+    WHEN("et element legges til foran i listen") {
 
-            list.push_front(5);
+      list.push_front(5);
 
-            THEN( "kan man se på det første elementet" ) {
-                REQUIRE( list.front().value() == 5 );
-            }
-        }
-        WHEN( "flere element legges foran i listen" ) {
-
-            list.push_front(5);
-            list.push_front(10);
-            list.push_front(15);
-
-            THEN( "er det siste elementet som ligger foran" ) {
-                REQUIRE( list.front().value() == 15 );
-            }
-        }
+      THEN("kan man se på det første elementet") {
+        REQUIRE(list.front().value() == 5);
+      }
     }
+    WHEN("flere element legges foran i listen") {
+
+      list.push_front(5);
+      list.push_front(10);
+      list.push_front(15);
+
+      THEN("er det siste elementet som ligger foran") {
+        REQUIRE(list.front().value() == 15);
+      }
+    }
+  }
 }
 
-SCENARIO( "elementer kan fjernes fra starten av listen", "[List]" ) {
+SCENARIO("elementer kan fjernes fra starten av listen", "[List]") {
 
-    GIVEN( "En liste som ikke er tom" ) {
-        List<int> list;
+  GIVEN("En liste som ikke er tom") {
+    List<int> list;
 
-        list.push_front(5);
-        list.push_front(10);
-        list.push_front(15);
+    list.push_front(5);
+    list.push_front(10);
+    list.push_front(15);
 
-        REQUIRE( list.front().has_value() );
+    REQUIRE(list.front().has_value());
 
-        WHEN( "et element fjernes fra starten av listen" ) {
+    WHEN("et element fjernes fra starten av listen") {
 
-            int v = list.pop_front().value();
+      int v = list.pop_front().value();
 
-            THEN( "får man tilbake det siste elementet som ble lagt til listen" ) {
-                REQUIRE( v == 15 );
-            }
+      THEN("får man tilbake det siste elementet som ble lagt til listen") {
+        REQUIRE(v == 15);
+      }
 
-            THEN( "er det neste elementet på listen foran på listen" ) {
-                REQUIRE( list.front().value() == 10 );
-            }
-        }
-        WHEN( "alle elementene fjernes fra listen" ) {
-
-            REQUIRE( list.pop_front().has_value() );
-            REQUIRE( list.pop_front().has_value() );
-            REQUIRE( list.pop_front().has_value() );
-
-            THEN( "er det første elementet tomt" ) {
-                REQUIRE( !list.front().has_value() );
-            }
-
-            THEN( "skjer det ingenting dersom man prøver å fjerne flere elementer" ) {
-                REQUIRE( !list.pop_front().has_value() );
-                REQUIRE( !list.pop_front().has_value() );
-                REQUIRE( !list.pop_front().has_value() );
-
-                REQUIRE( !list.front().has_value() );
-            }
-        }
+      THEN("er det neste elementet på listen foran på listen") {
+        REQUIRE(list.front().value() == 10);
+      }
     }
+    WHEN("alle elementene fjernes fra listen") {
+
+      REQUIRE(list.pop_front().has_value());
+      REQUIRE(list.pop_front().has_value());
+      REQUIRE(list.pop_front().has_value());
+
+      THEN("er det første elementet tomt") {
+        REQUIRE(!list.front().has_value());
+      }
+
+      THEN("skjer det ingenting dersom man prøver å fjerne flere elementer") {
+        REQUIRE(!list.pop_front().has_value());
+        REQUIRE(!list.pop_front().has_value());
+        REQUIRE(!list.pop_front().has_value());
+
+        REQUIRE(!list.front().has_value());
+      }
+    }
+  }
 }
